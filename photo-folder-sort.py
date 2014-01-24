93101b64d80f76dc5ecf5f2202c7a0d8e1cdf637
Arthur Blair <adblair@gmail.com>
2014-01-24 23:09:08 +0000

    another different approach

--- photo-folder-sort.py
+++ photo-folder-sort.py
@@ -1,60 +1,83 @@
 import datetime
+import logging
 import os
-import sys
 import pprint
+import re
+import sys
 
 import pyexiv2
 
-class Photo(object):
-    
-    def __init__(self, path):
-        """Raises IOError if a valid image path isn't given"""
-        self.path = path
-        self.name = os.path.basename(path)
-        self.meta = pyexiv2.metadata.ImageMetadata(path)
-        self.meta.read()
-        # print self.path, self.datetime
-        if self.name == 'berlin 2002 - 06.jpg':
-            pprint.pprint(self.meta.keys())
-            print self.meta.get('Exif.Image.ExifTag')
-            print self.meta.get('Exif.Image.DateTime') #Not the right one (there doesn't seem to be one!!)
-            print self.meta.get('Exif.Image.DateTimeOriginal')
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
 
 
-    @property
-    def datetime(self):
-        timestamp = getattr(self.meta.get('Exif.Image.DateTimeOriginal'), 'value', None)
-        return timestamp if isinstance(timestamp, datetime.datetime) else None
+def rget(mapping_object, keys, default=None):
+    """Return the value from a dict-like object for the first valid key in the
+    given seqence of keys, or else a default value if none of the keys is set.
+
+    @param mapping_object: A dict-like object (must have a .get() method)
+    @param keys: A sequence of keys to try in order
+    @param default: The default value to return, if none of the keys is present
+    """
+    next = rget(mapping_object, keys[1:]) if len(keys) > 1 else default
+    return mapping_object.get(keys[0], next)
 
 
-class PhotoDirectory(list):
+def get_photo_metadata(filepath):
+    """Return the ImageMetadata object for a given filepath.  Return False if
+    the filepath does not exist or is not an image file.
+    """
+    meta = pyexiv2.ImageMetadata(filepath)
+    try:
+        meta.read()
+    except IOError:
+        return False
+    return meta
 
-    def __init__(self, path):
-        self.path = path
-        self.extend(self.load_photos(path))
 
-    def load_photos(self, path):
+def get_photo_timestamp(filepath):
+    """Return the timestamp on which a given photo was taken."""
+    meta = get_photo_metadata(filepath)
+
+    tags = [
+        'Xmp.exif.DateTimeOriginal',
+        'Exif.Photo.DateTimeOriginal',
+    ]
+
+    if meta:
+        return getattr(rget(meta, tags), 'value', None)
+
+
+def set_folder_dates_to_median(path):
+
+        logger.info('Setting timestamps for {0}'.format(path))
+
         dirpath, dirnames, filenames = os.walk(path).next()
-        for filename in filenames:
-            try:
-                yield Photo(os.path.join(dirpath, filename))
-            except IOError:
-                pass
 
-    def set_modified_timestamp_to_median_photo_timestamp(self):
-        dates = sorted([img.datetime for img in self if img.datetime is not None])
-        if dates:
-            median_timestamp = dates[int(len(dates)/2)]
+        timestamps = [get_photo_timestamp(os.path.join(dirpath, fname)) for fname in filenames]
+        timestamps = sorted([tstamp.replace(tzinfo=None) for tstamp in timestamps if tstamp])
+
+        if timestamps:
+
+            median_timestamp = timestamps[int(len(timestamps)/2)]
+
+            """Generate integers (number of seconds since epoch) to represent
+            the Accessed and Modified timestamps, to be set for this folder.
+            """
             epoch = datetime.datetime.utcfromtimestamp(0)
             atime = (datetime.datetime.today() - epoch).total_seconds()
             mtime = (median_timestamp - epoch).total_seconds()
-            os.utime(self.path, (atime, mtime))
 
-def set_all_dirs(path):
-    for dirpath, dirnames, filenames in os.walk(path):
-        PhotoDirectory(dirpath).set_modified_timestamp_to_median_photo_timestamp()
+            os.utime(path, (atime, mtime))
+            logger.debug("Set Accessed time for {0} to {1}".format(path, atime))
+            logger.debug("Set Modified time for {0} to {1}".format(path, mtime))
+
+        else:
+            logger.warning("No valid timestamps found for {0}".format(path))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
-    set_all_dirs(sys.argv[1])
+    for dirpath, dirnames, filenames in os.walk(sys.argv[1]):
+        set_folder_dates_to_median(dirpath)
